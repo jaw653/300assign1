06e8f5c8f73e82b12a02b7047918e6fd046f1bc9
jaw653 <jawachs@crimson.ua.edu>
2018-09-19 10:20:12 -0500

    added full command support (not just one arg) to deter tokenize for ! feature

--- assign1.c
+++ assign1.c
@@ -7,18 +7,16 @@
 
 /**
  * Notes:
- * 		- While loop in history child process, if given break; will not reach parent process. without break, it will
-
- * 		- Should I add commands that were executed by ! or !! to the history?
+ * 		- Make sure history will only print 10 correct entries
+ *
+ * 		- Should incorrect commands such as "test" be added to history?
+ *
+ * 		- Final thing to do is add '&' functionality
  *
  * 		- Maybe change historyArr to only hold the most recent 10 cause prof could overload if lots of test cases
  *
- * 		- Exit functionality still not working
- *			1. currently only doesn't work when run right after the history function (takes as many times as you issued history to exit)
- *			2. make sure it handles 'exit' and 'Exit'
- *			3. when you misspell test after typing it twice the program hangs up. If you type during the hang then attempt to exit later it takes 2 tries
+ * 		- Make sure exit doesn't need to handle "Exit" as well
  *
- * 		- Must still add functionality for '&' w/ wait()
  *
  */
 
@@ -55,19 +53,23 @@ int main(void) {
 		fgets(input, 10000, stdin);
 		input[strlen(input)-1] = '\0';
 
+
 		/* Keep a copy of the input string that's not tokenized for history conditional */
 		char tmp[10000];
 		strcpy(tmp, input);
 
+
 		tokenize(input, args);
+
 
 		if (strcmp(args[0], "exit") == 0)
 			exit(0);
 
 		else if (strcmp("history", args[0]) != 0 && strcmp("!!", args[0]) != 0
-			&& strcmp("!", args[0]) != 0) {
-				printf("command %d\n", command_id);
+			&& args[0][0] != '!') {
+
 				strcpy(historyArr[command_id], tmp);
+				printf("copying in: %s\n", tmp);
 				command_id += 1;
 				execute(args);
 		}
@@ -84,124 +86,42 @@ int main(void) {
 				}
 			}
 			else if (strcmp("!!", args[0]) == 0) {
-				char *old_args[MAX_LINE/2+1];
-				tokenize(historyArr[command_id-1], old_args);
-				execute(old_args);
-			}
-			else if (strcmp("!", args[0]) == 0) {
-				char *old_args[MAX_LINE/2+1];
-				int index = atoi(args[1]);
-
-				tokenize(historyArr[index], old_args);
-				execute(old_args);
-			}
-		}
-
-		
-
-/*
-		/* Split string by spaces, send tokens to args[] var *
-		char *str = strtok(input, " ");
-		int i;
-		for (i = 0; str; i++) {
-			//printf("%s\n", str);
-			str[strlen(str)] = '\0';
-			args[i] = str;
-			str = strtok(NULL, " ");
-		}
-
-
-		/* Fill all remaining entries in args[] arr w/ NULL *
-		for (int j = i; j < MAX_LINE/2 + 1; j++)
-			args[j] = NULL;
-
-
-		/* Store command in historyArr, increment size counter *
-		if (strcmp("history", args[0]) != 0 && strcmp("!!", args[0]) != 0
-			&& strcmp("!", args[0]) != 0) {
-				strcpy(historyArr[command_id], tmp);
-				command_id += 1;
-		}
-
-
-		if (strcmp(args[i-1], "&") == 0) {			// FIXME: need to do add this functionality
-			printf("flag\n");
-			wait(NULL);
-		}
-
-		/* Catch exit before executing anything *
-		if (strcmp(args[0], "exit") == 0) {
-			should_run = 0;
-			exit(0);
-		}
-
-		/* Create child process *
-		process = fork();
-
-		if (process < 0) {
-			perror("fork() error.\n");
-			exit(-1);
-		}
-		
-		/* Child process *
-		else if (process == 0) {
-/*
-			if (strcmp(args[i-1], "&") == 0)
-				wait(NULL);
-*
-			if (strcmp(args[0], "exit") == 0) {
-				should_run = 0;
-				exit(0);
-			}
-
-			else if (strcmp(args[0], "history") == 0) {
-				int index = command_id-1;
-				//int x;
-				int counter = 1;
-				while (counter <= 10 && index > 0) {
-					//if (counter == command_id) break;
-					printf("%d %s\n", --index, historyArr[index]);
-					counter += 1;
+				if (command_id == 1) {
+					printf("No commands in history.\n");
 				}
-				printf("end flag\n");
-/*
-				for (x = command_id-1; x >= 1; x--) {
-					if (counter == 10) break;
-					printf("%d %s\n", --index, historyArr[x]);
-					counter += 1;
+				else {
+					char *old_args[MAX_LINE/2+1];
+					tokenize(historyArr[command_id-1], old_args);
+					
+					execute(old_args);
+
+					strcpy(historyArr[command_id], tmp);
+					command_id += 1;
 				}
-*
 			}
-
-			/* Execute most recent command in history *
-			else if (strcmp(args[0], "!!") == 0) {
+			else if (args[0][0] == '!') {
 				char *old_args[MAX_LINE/2+1];
-				tokenize(historyArr[command_id-1], old_args);
-				execvp(old_args[0], old_args);
-			}
+				int index = args[0][1] - '0';
 
-			/* Execute n'th command in history *
-			else if (strcmp(args[0], "!") == 0) {
-				char *old_args[MAX_LINE/2+1];
-				int index = atoi(args[1]);
+				if (index >= command_id || index < 1) {
+					printf("No such command in history.\n");
+				}
+				else {
+					char full_cmd[10000];
+					char full_cmd2[10000];
 
-				tokenize(historyArr[index], old_args);
-				execvp(old_args[0], old_args);
-			}
+					strcpy(full_cmd, historyArr[index]);
+					strcpy(full_cmd2, historyArr[index]);
 
-			printf("executing: %s\n", args[0]);
-			if (execvp(args[0], args) < 0)
-				printf("FAIL\n");
-		}
-		else {
-			wait(NULL);
-/*
-			if (strcmp(args[0], "exit") == 0) {
-				printf("exit flag\n");
-				break;
+					tokenize(full_cmd, old_args);
+					
+					execute(old_args);
+
+					printf("historyArr[index] is: %s\n", historyArr[index]);
+					strcpy(historyArr[command_id], full_cmd2);
+					command_id += 1;
+				}
 			}
-*
-			printf("parent\n");
 		}
 
 		/**
@@ -250,6 +170,10 @@ static void tokenize(char *input, char *arr[]) {
 	return;
 }
 
+/**
+ * Executes commands stored in args and creates child processe(s)
+ * @args - command to be executed
+ */
 static void execute(char **args) {
 	pid_t pid;
 	int status;
