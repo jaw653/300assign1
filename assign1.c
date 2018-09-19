fa7fedaceac2b8e324e9a4a1b041a27d7e159ddd
jaw653 <jawachs@crimson.ua.edu>
2018-09-18 19:57:02 -0500

    working with history array

--- assign1.c
+++ assign1.c
@@ -7,6 +7,9 @@
 
 /**
  * Notes:
+ * 		- Exit functionality still not working
+ *			1. takes as many exits as forks to quit
+ *			2. make sure it handles 'exit' and 'Exit'
  * 		- Must still add functionality for '&' w/ wait()
  *
  * 		- Must add history functionality that supports
@@ -22,10 +25,17 @@
 #include <sys/wait.h>
 
 #define MAX_LINE 80		// Max length command
+char historyArr[10000][10000];		// Array to store history
+int command_id = 0;					// FIXME: should it start at 1 or 0
+
+
+static void printHistory(char **, int);
+
 
 int main(void) {
 	char *args[MAX_LINE/2 + 1];		// Command line arguments
 	int should_run = 1;				// program exit flag
+
 
 	char input[10000];
 
@@ -37,6 +47,11 @@ int main(void) {
 
 		fgets(input, 10000, stdin);				// Scan entire line to input var
 		input[strlen(input)-1] = '\0';			// Get rid of trailing '\n'
+
+		// historyArr[command_id] = input;			// Store command in history
+		strcpy(historyArr[command_id], input);
+		printf("adding %s to historyArr\n", input);
+		command_id += 1;
 
 
 		char *str = strtok(input, " ");			// Split String by spaces
@@ -52,15 +67,17 @@ int main(void) {
 			args[j] = NULL;
 		}
 
-		printf("args[i-1] is: %s\n", args[i-1]);
+		// printf("args[i-1] is: %s\n", args[i-1]);  //purely for testing
 
 		process = fork();						// Create child process
 
-		if (strcmp(args[i-1], "&") == 0) {
+		if (strcmp(args[i-1], "&") == 0) {			// FIXME: need to do add this functionality
 			wait(NULL);
 		}
 
+
 		if (strcmp(args[0], "exit") == 0) {
+			should_run = 0;
 			exit(0);
 		}
 		else if (process < 0) {
@@ -72,8 +89,19 @@ int main(void) {
 				wait(NULL);
 
 			if (strcmp(args[0], "exit") == 0) {
+				should_run = 0;
 				exit(0);
+				//return 0;
 			}
+			else if (strcmp(args[0], "history") == 0) {
+				printf("flag\n");
+				//printHistory(historyArr, command_id);
+				int x;
+				for (x = 0; x < command_id; x++) {
+					printf("%s\n", historyArr[x]);
+				}
+			}
+
 			execvp(args[0], args);
 		}
 		else {
@@ -143,4 +171,13 @@ int main(void) {
 	}
 
 	return 0;
+}
+
+static void printHistory(char **commandHistory, int size) {
+	int i;
+	for (i = 0; i < size; i++) {
+		printf("%s\n", commandHistory[i]);
+	}
+
+	return;
 }
